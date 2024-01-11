69f0e4d1e052f7c6ce17b0a98c052ad4ef37af22
Mike Upton <me@uptonm.dev>
2024-01-10 23:17:19 -0500

    feat: improve seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "~/styles/globals.css";
 import { ConfigProvider } from "antd";
 import { Inter } from "next/font/google";
 import { headers } from "next/headers";
+import type { Metadata } from "next";
 
 import { ClientContainer } from "~/app/_components/shared/containers/ClientContainer";
 import StyledComponentsRegistry from "~/lib/StyledComponentsRegistry";
@@ -13,10 +14,32 @@ const inter = Inter({
   variable: "--font-sans",
 });
 
-export const metadata = {
-  title: "App Router Sandbox",
-  description: "A web application for testing out Next.js App Router",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+export const metadata: Metadata = {
+  title: "Budget",
+  description:
+    "A simple budgeting application for those too lazy for spreadsheets.",
+  icons: [
+    { rel: "icon", url: "/favicon.ico" },
+    { rel: "apple-touch-icon", sizes: "180x180", url: "/apple-touch-icon.png" },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+    { rel: "mask-icon", url: "/safari-pinned-tab.svg", color: "#5bbad5" },
+  ],
+  appleWebApp: {
+    title: "Budget",
+  },
+  applicationName: "Budget",
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
