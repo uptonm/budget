29d96d5ce09ba7efadb91490564aeebad8b56401
Mike Upton <me@uptonm.dev>
2024-01-13 16:27:00 -0500

    feat: dashboard placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Card, Col, Row } from "antd";
 import Title from "antd/es/typography/Title"; // TODO: Optimize imports
 
 import { ContentContainer } from "~/app/_components/shared/containers/ContentContainer";
@@ -11,10 +12,30 @@ export default function HomePage() {
         <ContentContainer
           header={
             <Title level={2} className="mb-0">
-              Home
+              Dashboard
             </Title>
           }
-        />
+        >
+          <Row gutter={16} className="h-full pt-4">
+            <Col span={12}>
+              <div className="flex h-full w-full flex-col items-center justify-stretch space-y-4">
+                <Card title="Monthly Spending" className="w-full flex-grow" />
+                <Card
+                  title="Transactions To Review"
+                  className="w-full flex-grow"
+                />
+                <Card title="Monthly Income" className="w-full flex-grow" />
+              </div>
+            </Col>
+            <Col span={12}>
+              <div className="flex h-full w-full flex-col items-center justify-stretch space-y-4">
+                <Card title="Net Worth" className="w-full flex-grow" />
+                <Card title="Top Categories" className="w-full flex-grow" />
+                <Card title="Next Two Weeks" className="w-full flex-grow" />
+              </div>
+            </Col>
+          </Row>
+        </ContentContainer>
       </SidebarContainer>
     </ProtectedRoute>
   );
